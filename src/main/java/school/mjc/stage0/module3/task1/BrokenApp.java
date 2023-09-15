c403b7232989710216a00dd60c678712fa22b665
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-15 10:05:29 +0600

    Update BrokenApp.java

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class BrokenApp {
     public static void main(String args) {
-        System.out,println("fixMe)
+        System.out.println("fix");
+        System.out.println("Me");
     }
 }
